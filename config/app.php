6a65c45d687c1b672fc966dd9fc634ec62151103
NRH <n.riccahang674@gmail.com>
2023-03-28 20:44:05 +0300

    update 28/03/2023

--- config/app.php
+++ config/app.php
@@ -179,6 +179,8 @@ return [
         App\Providers\RepositoryServiceProvider::class,
         App\Providers\ViewComposerServiceProvider::class,
 
+        Darryldecode\Cart\CartServiceProvider::class
+
 
     ],
 
@@ -231,6 +233,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Cart' => Darryldecode\Cart\Facades\CartFacade::class
 
     ],
 
